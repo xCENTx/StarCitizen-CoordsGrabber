bd1820b272bcd78cc74c4b49b268d07747071c7e
NightFyre <80198020+xCENTx@users.noreply.github.com>
2021-11-03 13:34:19 -0400

    Added Assistants
    - Hotkeys to move character
    - Textboxes for fine tuning character movement
    - Timer for if/when the game crashes , user will not have to restart the assistant
    - updated log output

--- src2(logger)/Form1.cs
+++ src2(logger)/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
+using helpers.StarCitizen_DevTool;
 using Memory;
 
 namespace StarCitizen_DevTool
@@ -9,6 +10,11 @@ namespace StarCitizen_DevTool
     {
         bool GameRunning;
         Mem m = new Mem();
+
+        Double varX;
+        Double varY;
+        Double varZ;
+
 
         public static class Offsets
         {
@@ -25,16 +31,7 @@ namespace StarCitizen_DevTool
 
         private void Main_Load(object sender, EventArgs e)
         {
-            int PID = m.GetProcIdFromName("StarCitizen");
-            if (PID > 0)
-            {
-                m.OpenProcess(PID);
-                GameRunning = true;
-                active_Panel.BackColor = Color.FromArgb(0, 169, 0);
-                return;
-            }
-            GameRunning = false;
-            active_Panel.BackColor = Color.FromArgb(169, 0, 0);
+
         }
 
         private void tele_button_Click(object sender, EventArgs e)
@@ -57,9 +54,94 @@ namespace StarCitizen_DevTool
                 return;
             }
             richTextBox.Text = "//" + summary_textBox.Text + "\n"
-                    + X_textBox.Text + "\n"
-                    + Y_textBox.Text + "\n"
-                    + Z_textBox.Text;
+                    + "coords[0] = " + X_textBox.Text + ";" + "\n"
+                    + "coords[1] = " + Y_textBox.Text + ";" + "\n"
+                    + "coords[2] = " + Z_textBox.Text + ";";
+        }
+
+        private void ProcessTimer_Tick(object sender, EventArgs e)
+        {
+            int PID = m.GetProcIdFromName("StarCitizen");
+            if (PID > 0)
+            {
+                m.OpenProcess(PID);
+                GameRunning = true;
+                active_Panel.BackColor = Color.FromArgb(0, 169, 0);
+                return;
+            }
+            GameRunning = false;
+            active_Panel.BackColor = Color.FromArgb(169, 0, 0);
+        }
+
+        //Set Hotkey NoClip Movement Strength
+        private void hotkey_button_Click(object sender, EventArgs e)
+        {
+            varX = Double.Parse(x_strengthBox.Text);
+            varY = Double.Parse(y_strengthBox.Text);
+            varZ = Double.Parse(z_strengthBox.Text);
+        }
+
+        private void HotkeyTimer_Tick(object sender, EventArgs e)
+        {
+            if (!GameRunning) return;
+            if (!hotkeys_checkBox.Checked) return;
+            if (varX == 0 || varY == 0 || varZ == 0) return;
+
+            
+            // ASCEND
+            if (func.GetAsyncKeyState(func.VK_NUMPAD1) != 0)
+            {
+                Double origin = m.ReadDouble($"StarCitizen.exe+{Offsets.Z}");
+                Double newVALUE = origin + varZ;
+                string parseVALUE = newVALUE.ToString();
+                m.WriteMemory($"StarCitizen.exe+{Offsets.Z}", "double", parseVALUE);
+            }
+            
+            // DESCEND
+            if (func.GetAsyncKeyState(func.VK_NUMPAD0) != 0)
+            {
+                Double origin = m.ReadDouble($"StarCitizen.exe+{Offsets.Z}");
+                Double newVALUE = origin - varZ;
+                string parseVALUE = newVALUE.ToString();
+                m.WriteMemory($"StarCitizen.exe+{Offsets.Z}", "double", parseVALUE);
+            }
+
+            //+
+            if (func.GetAsyncKeyState(func.VK_UP) != 0)
+            {
+                Double origin = m.ReadDouble($"StarCitizen.exe+{Offsets.X}");
+                Double newVALUE = origin + varX;
+                string parseVALUE = newVALUE.ToString();
+                m.WriteMemory($"StarCitizen.exe+{Offsets.X}", "double", parseVALUE);
+            }
+
+            //-
+            if (func.GetAsyncKeyState(func.VK_DOWN) != 0)
+            {
+                Double origin = m.ReadDouble($"StarCitizen.exe+{Offsets.X}");
+                Double newVALUE = origin - varX;
+                string parseVALUE = newVALUE.ToString();
+                m.WriteMemory($"StarCitizen.exe+{Offsets.X}", "double", parseVALUE);
+            }
+
+            //-
+            if (func.GetAsyncKeyState(func.VK_LEFT) != 0)
+            {
+                Double origin = m.ReadDouble($"StarCitizen.exe+{Offsets.Y}");
+                Double newVALUE = origin - varY;
+                string parseVALUE = newVALUE.ToString();
+                m.WriteMemory($"StarCitizen.exe+{Offsets.Y}", "double", parseVALUE);
+            }
+
+            //+
+            if (func.GetAsyncKeyState(func.VK_RIGHT) != 0)
+            {
+                Double origin = m.ReadDouble($"StarCitizen.exe+{Offsets.Y}");
+                Double newVALUE = origin + varY;
+                string parseVALUE = newVALUE.ToString();
+                m.WriteMemory($"StarCitizen.exe+{Offsets.Y}", "double", parseVALUE);
+            }
         }
     }
 }
+
